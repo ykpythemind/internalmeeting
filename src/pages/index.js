4ea3fd9648185c229f149d8c80a6c50ded894a9e
Yukito Ito <yukibukiyou@gmail.com>
2020-04-11 17:08:52 +0900

    ya

---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+import "./index.css"
+
+const IndexPage = () => {
+  return (
+    <>
+      <div class="main">
+        <div>INTERNAL MEETING</div>
+      </div>
+      <a
+        class="cllctv"
+        href="http://cllctv-jp.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        {" "}
+      </a>
+    </>
+  )
+}
+
+export default IndexPage
